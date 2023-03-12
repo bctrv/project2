d49ded84c6f71588038bb80cf4f6d8b445f23eb0
artem bystrov <ockidocki1@gmail.com>
2023-03-12 20:25:44 +0400

    modernized

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        int cost = 20000;
-        int bonus = 20;
-
-        System.out.println(cost / bonus);
-
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
