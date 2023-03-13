10fe80b98ec58c1d1b34fa9802650e48bacb6b35
artem bystrov <ockidocki1@gmail.com>
2023-03-13 16:16:14 +0400

    update

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,9 +2,9 @@ public class BonusMilesService {
 
     public int calculate(int price) {
         int result;
-        {
-            result = price / 20;
-        }
+
+        result = price / 20;
+
         return result;
 
     }
